410ff11bb04001764b389df12d179f522ced96b3
Khani <nikzad@IST-HC-MD-0020.ad.bu.edu>
2019-08-07 19:19:37 -0400

    added mergesort

--- Sort.py
+++ Sort.py
@@ -37,18 +37,80 @@ def find_index_min(ls):
             min_index = i
     return min_index
 
+def merge_sort(ls):
+    """ Implementation of the Merge Sort Algorithm:
+        Logarithmic Move Time: O(nlog(n) Logarithmic Comparison Time: O(nlog(n)
+        Overall Time: O(nlog(n)) + O(nlog(n)) = O(nlog(n))
+    """
+    temp = [0] * len(ls)
+    m_sort(ls, temp, 0, len(ls) - 1)
 
-# ls = [random.randint(0,50) for i in range(20)]
-# print(ls)
-# 
-# print('\nSelection Sort:')
-# selection_sort(ls)
-# print(ls)
+def m_sort(ls, temp, start, end):
+    """ Recursion of the Merge Sort Algorithm
+        Helper function that handles the recursion, so the wrapper doesn't
+        have to.
+    """
+    if (start <= end):
+        return
 
-# print('\nShuffled Again:')
-# random.shuffle(ls)
-# print(ls)
-#
-# print('\nBubble Sort:')
-# bubble_sort(ls)
-# print(ls)
+    middle = (start + end) // 2
+
+    m_sort(ls, temp, start, middle)
+    print("poop")
+    m_sort(ls, temp, middle + 1, end)
+
+    i = start
+    j = middle + 1
+    k = start
+
+    left_start = start
+    left_end = middle
+    right_start = middle + 1
+    right_end = end
+
+    while i <= left_end and j <= right_end:
+        if ls[i] < ls[j]:
+            temp[k] = ls[i]
+            i += 1
+            j += 1
+        else:
+            temp[k] = ls[j]
+            j += 1
+            k += 1
+
+    while i <= left_end:
+        temp[k] = ls[i]
+        i += 1
+        k += 1
+
+    while j <= right_end:
+        temp[k] = ls[j]
+        j += 1
+        k += 1
+
+    for i in range(left_start, right_end + 1):
+        ls[i] = temp[i]
+
+
+ls = [random.randint(0,50) for i in range(20)]
+print(ls)
+
+print('\nSelection Sort:')
+selection_sort(ls)
+print(ls)
+
+print('\nShuffled Again:')
+random.shuffle(ls)
+print(ls)
+
+print('\nBubble Sort:')
+bubble_sort(ls)
+print(ls)
+
+print('\nShuffled Again:')
+random.shuffle(ls)
+print(ls)
+
+print('\nMerge Sort:')
+merge_sort(ls)
+print(ls)
